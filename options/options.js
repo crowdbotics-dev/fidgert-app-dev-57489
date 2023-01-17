387d352c5612960f34bd79ad391b2039f0551b5b
Crowdbotics <engineering@crowdbotics.com>
2023-01-17 15:39:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fidgert-app-dev-57489",
+  url: "https://fidgert-app-dev-57489.botics.co",
+  api: "https://fidgert-app-dev-57489.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
